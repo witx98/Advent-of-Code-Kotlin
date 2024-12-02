df211d5ab30065922ab7021b2504321e62748ac7
witx98 <witx90@gmail.com>
2024-12-02 22:51:39 +0100

    1 day tasks

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,34 @@
+import kotlin.math.abs
+
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val lines = readInput("day-01-input")
+    val firstResult = firstPart(lines)
+    val secondResult = secondPart(lines)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    firstResult.println()
+    secondResult.println()
+}
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+private fun firstPart(lines: List<String>): Long {
+    val (left, right) = lines.map { line ->
+        val first = line.substringBefore(" ").toLong()
+        val second = line.substringAfterLast(" ").toLong()
+        first to second
+    }.unzip()
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    return left.sorted().zip(right.sorted()).sumOf { (first, second) -> abs(first - second) }
+}
+
+private fun secondPart(lines: List<String>): Long {
+    val (left, right) = lines.map { line ->
+        val first = line.substringBefore(" ").toLong()
+        val second = line.substringAfterLast(" ").toLong()
+        first to second
+    }.unzip()
+
+    val frequency: Map<Long, Int> = right.groupingBy { it }.eachCount()
+
+    return left.fold(0L) { acc, num -> acc + num * frequency.getOrDefault(num, 0) }
 }
