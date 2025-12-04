0f2a1e33ca3edec16ac27a0f828f68db09465e41
witx98 <witx90@gmail.com>
2025-12-04 22:52:34 +0100

    Day 1 first part

--- src/y2025/Day01.kt
+++ src/y2025/Day01.kt
@@ -5,7 +5,7 @@ import readInputLines
 
 
 fun main() {
-    val lines = readInputLines(2024, "day-01-input")
+    val lines = readInputLines(2025, "day-01-input")
     val firstResult = firstPart(lines)
     val secondResult = secondPart(lines)
 
@@ -15,10 +15,27 @@ fun main() {
 
 fun secondPart(lines: List<String>) {
 
-
 }
 
-fun firstPart(lines: List<String>) {
+fun firstPart(lines: List<String>): Int {
+    var current = 50
+    var counter = 0
+    lines.forEach {
+        val (first, second) = parseLine(it)
+        current = if (first == 'L') {
+            (current - second) % 100
+        } else {
+            (current + second) % 100
+        }
 
 
+        if (current == 0) {
+            counter++
+        }
+    }
+    return counter
+}
+
+fun parseLine(line: String): Pair<Char, Int> {
+ return line[0] to line.substring(1).toInt()
 }
