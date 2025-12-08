c0326e4fae45974e5344fbf91481cad407fb708e
Mateusz Witkowski <mateusz.witkowski@softnet.com.pl>
2025-12-08 17:07:50 +0100

    Prepare for 9,10,11,12 days

---
+++ src/y2025/Day09.kt
@@ -0,0 +1,24 @@
+package y2025
+
+import println
+import readInputLines
+
+
+fun main() {
+    val lines = readInputLines(2025, "day-09-input")
+    val firstResult = firstPart(lines)
+    val secondResult = secondPart(lines)
+
+    firstResult.println()
+    secondResult.println()
+}
+
+private fun secondPart(lines: List<String>): Int {
+
+    return 0
+}
+
+private fun firstPart(lines: List<String>): Int {
+
+    return 0
+}
