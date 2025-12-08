22d60ae11161e8d88a0b2e502077136dc378d560
Mateusz Witkowski <mateusz.witkowski@softnet.com.pl>
2025-12-08 14:41:50 +0100

    Day 8 - refactor

--- src/y2025/Day08.kt
+++ src/y2025/Day08.kt
@@ -18,10 +18,45 @@ fun main() {
     secondResult.println()
 }
 
+private fun firstPart(positions: List<Position>): Int {
+    val connectionsSortedByDistance = connectionsSortedByDistance(positions)
+    val circuits = findCircuits(positions, connectionsSortedByDistance)
+
+    val biggestCircuits = circuits.sortedByDescending { it.size }.take(3)
+    val result = biggestCircuits
+        .map { it.size }
+        .reduce { acc, next -> acc * next }
+    return result
+}
+
 private fun secondPart(positions: List<Position>): Long {
     val connectionsSortedByDistance = connectionsSortedByDistance(positions)
     val (pos1, pos2) = findLastCircuit(positions, connectionsSortedByDistance)
     return pos1.x * pos2.x
+}
+
+private fun connectionsSortedByDistance(positions: List<Position>): List<Connection> {
+    val distances = mutableListOf<Pair<Position, Position>>()
+    for (i in 0 until positions.size - 1) {
+        for (j in i + 1 until positions.size) {
+            distances += positions[i] to positions[j]
+        }
+    }
+    return distances
+        .map { Connection(it.first, it.second) }
+        .sortedBy { it.distance }
+}
+
+private fun findCircuits(
+    positions: List<Position>,
+    connectionsSortedByDistance: List<Connection>
+): MutableSet<Set<Position>> {
+    val circuits = positions.map { setOf(it) }.toMutableSet()
+
+    for ((pos1, pos2) in connectionsSortedByDistance.take(1000)) {
+        circuits.update(pos1, pos2)
+    }
+    return circuits
 }
 
 private fun findLastCircuit(
@@ -51,43 +86,7 @@ fun MutableSet<Set<Position>>.update(pos1: Position, pos2: Position) {
     this.add(merge)
 }
 
-private fun firstPart(positions: List<Position>): Int {
-    val connectionsSortedByDistance = connectionsSortedByDistance(positions)
-    val circuits = findCircuits(positions, connectionsSortedByDistance)
-
-    val biggestCircuits = circuits.sortedByDescending { it.size }.take(3)
-    val result = biggestCircuits
-        .map { it.size }
-        .reduce { acc, next -> acc * next }
-    return result
-}
-
-private fun findCircuits(
-    positions: List<Position>,
-    connectionsSortedByDistance: List<Connection>
-): MutableSet<Set<Position>> {
-    val circuits = positions.map { setOf(it) }.toMutableSet()
-
-    for ((pos1, pos2) in connectionsSortedByDistance.take(1000)) {
-        circuits.update(pos1, pos2)
-    }
-    return circuits
-}
-
-
 private data class Connection(val pos1: Position, val pos2: Position) {
     val distance = pos1.distance(pos2)
-}
-
-private fun connectionsSortedByDistance(positions: List<Position>): List<Connection> {
-    val distances = mutableListOf<Pair<Position, Position>>()
-    for (i in 0 until positions.size - 1) {
-        for (j in i + 1 until positions.size) {
-            distances += positions[i] to positions[j]
-        }
-    }
-    return distances
-        .map { Connection(it.first, it.second) }
-        .sortedBy { it.distance }
 }
 
